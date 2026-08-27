a0c1b70a44c513d9577e9d2417f52677d1b2ed59
Bernardo Pinheiro <pinheirobernarddo085@gmail.com>
2026-08-26 23:23:57 -0300

    fix: ativa MongoDB embarcado (Flapdoodle) nos testes de contexto

--- src/test/java/com/hanei/financehub/api/HaneiFinancehubApiApplicationTests.java
+++ src/test/java/com/hanei/financehub/api/HaneiFinancehubApiApplicationTests.java
@@ -2,12 +2,14 @@ package com.hanei.financehub.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HaneiFinancehubApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
 
+    }
 }
